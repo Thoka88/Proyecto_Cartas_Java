85e199bb8e9d2ad45696e7617a6cdf7385755caf
iordix <yordir932@gmail.com>
2025-06-17 18:35:17 -0600

    Create CartaBonus.java
    ...

---
+++ Proyecto_Juego_Cartas/src/main/java/com/uisil/proyecto_juego_cartas/CartaBonus.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.uisil.proyecto_juego_cartas;
+
+/**
+ *
+ * @author Usuario
+ */
+public class CartaBonus {
+    //general
+    public void masCincoSeg(){
+    
+        
+        
+    }
+    //general
+    public void verCarta(){
+    
+        
+    
+    }
+    //modo dificil
+    public void mostrarPareja(){
+    
+        
+    
+    }
+    
+    public void verCarta(){
+    
+        
+    
+    }
+    
+}
