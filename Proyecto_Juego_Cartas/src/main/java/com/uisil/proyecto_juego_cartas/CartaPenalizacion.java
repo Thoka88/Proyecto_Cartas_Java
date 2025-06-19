029c4af421d01aa258038395032f5549a5f924ee
iordix <yordir932@gmail.com>
2025-06-18 18:36:13 -0600

    avance de bonus y penalizacion
    cree metodos

---
+++ Proyecto_Juego_Cartas/src/main/java/com/uisil/proyecto_juego_cartas/CartaPenalizacion.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.uisil.proyecto_juego_cartas;
+
+/**
+ *
+ * @author Yordi
+ */
+public class CartaPenalizacion {
+    //facil
+    public void menosCincoSeg(){
+    
+      //metodo que resta 5seg a el timer  
+        
+    }
+    //facil
+    public void menosUnPunto(){
+    
+        //resta 1 punto a el puntaje
+    
+    }
+    //medio
+    public void menosDiezSeg(){
+    
+        //metodo que resta 10seg a el timer 
+    
+    }
+    
+    //medio
+    public void verUnaCarta(){
+    
+        //Solo se puede mostrar 1 carta en los siguientes 2 turnos
+    
+    }
+
+    //modo dificil
+    public void menosTreintaSeg(){
+    
+        //metodo que resta 30seg a el timer
+    
+    }
+    //modo dificil
+    public void mezclarCartas(){
+    
+        //se mezclan las cartas no emparejadas
+    }
+    
+    
+}
