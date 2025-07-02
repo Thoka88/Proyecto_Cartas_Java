597c774137c8765f071dabb620ae846fe912873b
iordix <yordir932@gmail.com>
2025-07-02 15:26:24 -0600

    Update Juego.java

--- Proyecto_Juego_Cartas/src/main/java/com/uisil/proyecto_juego_cartas/logic/Juego.java
+++ Proyecto_Juego_Cartas/src/main/java/com/uisil/proyecto_juego_cartas/logic/Juego.java
@@ -57,9 +57,10 @@ public class Juego {
         Collections.shuffle(cartasNoEmparejadas);
         System.out.println("Cartas no emparejadas mezcladas.");
     }
+
     public void setTiempoRestante(int tiempoRestante) {
-    this.tiempoRestante = tiempoRestante;
-}
+        this.tiempoRestante = tiempoRestante;
+    }
 
     // Getters para pruebas o para otras clases
     public int getTiempoRestante() {
